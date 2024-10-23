63237e7c66cfd3943853f3f050450af8bab55e80
karina1121111 <karinsonia631@gmail.com>
2024-10-22 23:27:15 -0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+#verus
+sudo apt update
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz 
+tar -xvf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RQRrkVFUEF1FG3g51dhVjvssoW6HRGUGjZ.vps1 -p hybrid
