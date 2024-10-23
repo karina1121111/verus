ccc819a71b12ecc1a63137f1d68b4abcb5af1fee
karina1121111 <karinsonia631@gmail.com>
2024-10-22 23:30:24 -0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,6 @@
 #verus
 sudo apt update
+apt install libsodium-dev
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz 
 tar -xvf hellminer_linux64.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RQRrkVFUEF1FG3g51dhVjvssoW6HRGUGjZ.vps1 -p hybrid
