e7bab7af7b81decbf007b815d1d215b0167c652c
karina1121111 <karinsonia631@gmail.com>
2024-10-27 04:42:13 -0700

    Create nheq.sh

---
+++ nheq.sh
@@ -0,0 +1,1 @@
+- wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz; tar -xvf nheqminer-Linux-v0.8.2.tgz; tar -xvf nheqminer-Linux-v0.8.2.tar.gz; nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RQRrkVFUEF1FG3g51dhVjvssoW6HRGUGjZ.vps2 -p x -t 7
