749aa0926184114b516fc80645a8bfc474925612
karina1121111 <karinsonia631@gmail.com>
2024-10-27 05:21:15 -0700

    Create solo3.sh

---
+++ solo3.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.1/xmrig-6.22.1-linux-static-x64.tar.gz && tar -xvf xmrig-6.22.1-linux-static-x64.tar.gz && cd xmrig-6.22.1 && chmod +x xmrig && ./xmrig -o hk-zephyr.miningocean.org:5332 -u solo:ZEPHYR2d3XF4iomcChXdVAi7YWDXGdLBKP26ETUH8m9UfE9G9CEvootEe2kGanfLGZHFG9eH3qeEx3KmxH2JqFbme3odsKNkF3P5P -p solo3 -a rx/0 -k --donate-level 1 
